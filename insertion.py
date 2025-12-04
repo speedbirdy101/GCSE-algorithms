fb6d6dde7816d51a78097c35379113401aada748
Robin <100711366+speedbirdy101@users.noreply.github.com>
2025-12-04 13:12:38 +0000

    Add insertion sort implementation in insertion.py
    Initial implementation of insertion sort with an unsorted list.

---
+++ insertion.py
@@ -0,0 +1,10 @@
+# Insertion sort
+unsorted_list = ["Josh", "Aarish", "Alfred", "Will", "Lenud", "Harry", "Kobe"]
+
+
+def insertion_sort(to_sort):
+    for pointer in to_sort:
+        pass
+    
+
+print(insertion_sort(unsorted_list))
